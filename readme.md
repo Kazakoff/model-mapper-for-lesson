8aa4b99ce2b7af66875bf5eb39e2c0de27a4e49c
Kazakoff <wadimchik@mail.com>
2025-04-17 13:42:57 +0300

    Remake + Converter

---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/amdegregorio/ModelMapperExample/blob/master/src/main/java/amdegregorio/modelmapperexample/item/Item.java
--- src/main/java/by/vstu/isit/App.java
+++ src/main/java/by/vstu/isit/App.java
@@ -1,7 +1,6 @@
 package by.vstu.isit;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeMap;
+import org.modelmapper.*;
 
 import by.vstu.isit.models.Address;
 import by.vstu.isit.models.Person;
@@ -13,16 +12,23 @@ import by.vstu.isit.models.PersonDto;
 public class App {
     public static void main(String[] args) {
 
-        TypeMap<Person, PersonDto> personToDto = new ModelMapper().createTypeMap(Person.class, PersonDto.class)
-                .addMapping(Person::getName, PersonDto::setFirstName)
-                .addMapping(x -> x.getAddress().getCity(), PersonDto::setCity)
-                .addMapping(x -> x.getAddress().getStreet(), PersonDto::setStreet);
+        TypeMap<Person, PersonDto> personToDto = new ModelMapper().createTypeMap(Person.class, PersonDto.class);
+
+        Converter<String, String> toUppercase = new AbstractConverter<String, String>() {
+            protected String convert(String source) {
+                return source == null ? null : source.toUpperCase();
+            }
+        };
+        personToDto.addMappings(mapper -> mapper.using(toUppercase).map(Person::getName, PersonDto::setFirstName));
+        personToDto.addMapping(x -> x.getAddress().getCity(), PersonDto::setCity);
+        personToDto.addMapping(x -> x.getAddress().getStreet(), PersonDto::setStreet);
 
         PersonDto personDto = personToDto.map(Person.builder()
                 .name("Michal")
                 .age(15)
                 .address(new Address("Warsaw", "Nowy Swiat"))
                 .build());
+
         System.out.println(personDto);
     }
 }
