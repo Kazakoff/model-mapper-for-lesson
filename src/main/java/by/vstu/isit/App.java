b957db943c9c5e3ab8b43c433b48565cbe7dfaee
Kazakoff <wadimchik@mail.ru>
2025-04-23 00:03:40 +0300

    provider --

--- src/main/java/by/vstu/isit/App.java
+++ src/main/java/by/vstu/isit/App.java
@@ -6,26 +6,35 @@ import by.vstu.isit.models.Address;
 import by.vstu.isit.models.Person;
 import by.vstu.isit.models.PersonDto;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
+        System.out.println("______________Provider");
+        TypeMap<Person, Person> personToProviderDto = new ModelMapper().createTypeMap(Person.class, Person.class);
+        Provider<Person> personProvider = p -> {
+            System.out.println(p);
+            return Person.builder()
+                    .name("MMM")
+                    .age(1)
+                    .address(new Address("Vit", "MM st"))
+                    .build();
+        };
 
+        personToProviderDto.setProvider(personProvider);
+
+        Person personProviderDto = personToProviderDto.map(Person.builder()
+                .name("Michal")
+                .age(15)
+                .build());
+
+        System.out.println(personProviderDto);
+
+        System.out.println("______________Converter");
         TypeMap<Person, PersonDto> personToDto = new ModelMapper().createTypeMap(Person.class, PersonDto.class);
 
         Converter<String, String> toUppercase = new AbstractConverter<String, String>() {
             protected String convert(String source) {
                 return source == null ? null : source.toUpperCase();
             }
-        };
-
-        Provider<String> personProvider = new AbstractProvider<String>() {
-            public String get() {
-                return "Пользовательский поставщик";
-
-            }
-
         };
 
         personToDto.addMappings(mapper -> mapper.using(toUppercase).map(Person::getName, PersonDto::setFirstName));
